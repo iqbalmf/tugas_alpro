48d2b55161404be65e69611752f612d52c9db7d2
iqbalmf <iqbalmf68@gmail.com>
2019-10-09 23:36:08 +0700

    exception handling

---
+++ src/tradisional_exception.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class tradisional_exception {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan dua buah integer: ");
+        int angka1 = input.nextInt();
+        int angka2 = input.nextInt();
+
+        try{
+            if(angka2 == 0){
+                throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+            }else {
+                int hasil = angka1/angka2;
+                System.out.println("Hasil dari " + angka1 + " dibagi " + angka2
+                        + " adalah: " + hasil);
+            }
+        }
+
+        catch (ArithmeticException exception){
+            System.out.println("Exception: Suatu integer "
+                    + "tidak dapat dibagi oleh nol!");
+        }
+        finally {
+            System.out.println("Eksekusi program dilanjutkan ...");
+        }
+    }
+}
